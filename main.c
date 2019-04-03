7d164e74396b42c158d26723af289d061722f13a
devnanga <33191067+devnanga@users.noreply.github.com>
2019-04-03 11:49:02 -0400

    Worked on keyboard stuff

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <math.h>
+#include "address_map_arm.h"
 
 #define PI 3.14159265
 
@@ -11,6 +12,30 @@ void swap(int* a, int* b) {
     int temp = *a;
     *a = *b;
     *b = temp;
+}
+
+void HEX_PS2(char b1, char b2, char b3){
+
+    volatile int * HEX3_HEX0_ptr = (int *)HEX3_HEX0_BASE;
+    volatile int * HEX5_HEX4_ptr = (int *)HEX5_HEX4_BASE;
+
+    unsigned char seven_seg_decode_table[] = {
+    0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07,
+    0x7F, 0x67, 0x77, 0x7C, 0x39, 0x5E, 0x79, 0x71};
+    unsigned char hex_segs[] = {0, 0, 0, 0, 0, 0, 0, 0};
+    unsigned int shift_buffer, nibble;
+    unsigned char code;
+    int i;
+    shift_buffer = (b1 << 16) | (b2 << 8) | b3;
+    for (i = 0; i < 6; ++i) {
+    nibble = shift_buffer & 0x0000000F;
+    code = seven_seg_decode_table[nibble];
+    hex_segs[i] = code;
+    shift_buffer = shift_buffer >> 4;
+}
+
+*(HEX3_HEX0_ptr) = *(int *)(hex_segs);
+*(HEX5_HEX4_ptr) = *(int *)(hex_segs + 4);
 }
 
 void plot_pixel(int x, int y, short int line_color)
